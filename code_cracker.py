9620f49979e5d5066de888787ce9eab6835c787e
hugo <hugo.heefer@ns.nl>
2024-08-13 16:10:08 +0200

    refactored

--- code_cracker.py
+++ code_cracker.py
@@ -1,18 +1,16 @@
 def decode_or_encode(message_to_process: str, type: str ="decode") -> str:
     if type == "decode":
-        characters_for_input = "abcdefghijklmnopqrstuvwxyz"
-        characters_for_output = '!)"(£*%&><@abcdefghijklmno'
+        characters_for_input = "abcdefghijklmnopqrstuvwxyz "
+        characters_for_output = '!)"(£*%&><@abcdefghijklmno '
     elif type == "encode":
-        characters_for_input = '!)"(£*%&><@abcdefghijklmno'
-        characters_for_output = "abcdefghijklmnopqrstuvwxyz"
+        characters_for_input = '!)"(£*%&><@abcdefghijklmno '
+        characters_for_output = "abcdefghijklmnopqrstuvwxyz "
         
     if message_to_process == "":
         return ""
     else:
         processed_message = ""
         for character in message_to_process:
-            if character == " ":
-                    processed_message += " "
             for element in characters_for_output:
                 if character == element:
                     index = characters_for_output.index(character)
