faff057f46485c3ae27c74b88d86197f6c78351f
hugo <hugo.heefer@ns.nl>
2024-08-14 11:44:28 +0200

    4 tests

---
+++ code_cracker.py
@@ -0,0 +1,15 @@
+def decode(message: str) -> str:
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    decryption_key = '!)"(£*%&><@abcdefghijklmno'
+
+    if message == "":
+        return ""
+    else:
+        decoded_message = ""
+        for character in message:
+            for element in decryption_key:
+                if character == element:
+                    index = decryption_key.index(character)
+                    decoded_message += alphabet[index]
+    return decoded_message
+        
---
+++ test_application.py
@@ -0,0 +1,29 @@
+from code_cracker import decode
+
+def test_an_empty_message_gives_an_empty_decoded_message():
+    #given
+    message = ""
+
+    #then
+    assert decode(message) == ""
+ 
+def test_a_message_with_one_character_is_decoded_correctly():
+    #given
+    message = "!"
+
+    #then
+    assert decode(message) == "a"
+
+def test_a_message_with_two_characters_is_decoded_correctly():
+    #given
+    message = "!)"
+
+    #then
+    assert decode(message) == "ab"
+
+def test_a_message_with_three_characters_in_reverse_order_is_decoded_correctly():
+    #given
+    message = 'onm'
+
+    #then
+    assert decode(message) == "zyx"
